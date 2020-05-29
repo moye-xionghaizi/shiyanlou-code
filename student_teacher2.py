a8ac9db47dba28f6e25cab89102a3bd349ec1f4b
moye <xionghaizi>
2020-05-29 15:54:06 +0800

    20200529

--- student_teacher2.py
+++ student_teacher2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-from collections import Counter
-import sys
-
 
 class Person(object):
     """
@@ -16,9 +13,6 @@ class Person(object):
         返回包含人名的字符串
         """
         return self.name
-    def get_grade(self):
-        return 0
-
 
 
 class Student(Person):
@@ -26,28 +20,16 @@ class Student(Person):
     返回 Student 对象，采用 name, branch, year 3 个参数
     """
 
-    def __init__(self, name, branch, year, grade):
+    def __init__(self, name, branch, year):
         Person.__init__(self, name) # 子类 调用父类 Person 的 __init__ 方法
         self.branch = branch
         self.year = year
-        self.grade = grade
 
     def get_details(self):
         """
         返回包含学生具体信息的字符串
         """
         return "{} studies {} and is in {} year.".format(self.name, self.branch, self.year)
-    def get_grade(self):
-        # Pass:x,Fail:x
-        common = Counter(self.grade).most_common(4)
-        n1 = 0
-        n2 = 0
-        for item in common:
-            if item[0] !='D':
-                n1 += item[1]
-            else:
-                n2 += item[1]
-        print("Pass: {},Fail:{}".format(n1,n2))
 
 
 class Teacher(Person):
@@ -55,31 +37,18 @@ class Teacher(Person):
     返回 Teacher 对象，采用字符串列表作为参数
     """
 
-    def __init__(self, name, papers, grade):
+    def __init__(self, name, papers):
         Person.__init__(self, name)     #子类 调用父类 Person 的 __init__ 方法
         self.papers = papers
-        self.grade = grade
 
     def get_details(self):
         return "{} teaches {}".format(self.name, ','.join(self.papers))
 
-    def get_grade(self):
-        # A:x,B:x,C:x,D:x
-        s = []
-        common = Counter(self.grade).most_common(4)
-        for i,j in common:
-            s.append("{}:{}".format(i,j))
-        print(", ".join(s))
-
 
 person1 = Person('Sachin')
-if sys.argv[1] == "student":
-    student1 = Student('Kushal', 'CSE', 2005,sys.argv[2])
-    student1.get_grade()
-else:
-    teacher1 = Teacher('Prashad', ['C', 'C++'],sys.argv[2])
-    teacher1.get_grade()
+student1 = Student('Kushal', 'CSE', 2005)
+teacher1 = Teacher('Prashad', ['C', 'C++'])
 
-# print(person1.get_details())
-# print(student1.get_details())
-# print(teacher1.get_details())
+print(person1.get_details())
+print(student1.get_details())
+print(teacher1.get_details())
