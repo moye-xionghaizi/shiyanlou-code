521583d3f70f64978cf80f66bcecc0d9b302f257
moye <xionghaizi>
2020-06-01 15:54:58 +0800

    20200601

---
+++ factorial_test.py
@@ -0,0 +1,13 @@
+import unittest
+from factorial import fact,div
+
+class TestFactorial(unittest.TestCase):
+    def test_fact(self):
+        res = fact(5)
+        self.assertEqual(res,120)
+
+    def test_div(self):
+        self.assertRaises(ZeroDivisionError,div,0)
+
+if __name__ == "__main__":
+    unittest.main()
